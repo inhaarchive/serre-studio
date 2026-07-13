aa35c7f430aec2a0833b47aba11a7dc0f77a8a5f
Hyein Gwon <hyeinghi0234@gmail.com>
2026-07-13 18:02:53 +0900

    Update exhibitions.11tydata.js

--- src/content/exhibitions/exhibitions.11tydata.js
+++ src/content/exhibitions/exhibitions.11tydata.js
@@ -2,8 +2,19 @@ module.exports = {
   layout: "detail.njk",
   tags: ["exhibitions"],
   section_label: "Exhibition",
+
   eleventyComputed: {
-    permalink: data => data.status === "public" ? `/archive/${data.page.fileSlug}/index.html` : false,
-    eleventyExcludeFromCollections: data => data.status !== "public"
+    permalink: data => {
+      if (data.status !== "public") return false;
+
+      const fileName =
+        data.page?.filePathStem?.split("/").filter(Boolean).pop() ||
+        data.page?.fileSlug;
+
+      return `/archive/${fileName}/index.html`;
+    },
+
+    eleventyExcludeFromCollections: data =>
+      data.status !== "public"
   }
 };
